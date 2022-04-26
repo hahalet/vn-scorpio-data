93ce13dfd0f5776d519e7bd793ab332e16aa88d0
luolaigang <luolaigang@newstarfintech.com>
2022-04-26 10:07:30 +0800

    服务崩溃修改

--- scorpiodata/src/main/java/com/newstar/scorpiodata/services/MyFirebaseMessagingService.java
+++ scorpiodata/src/main/java/com/newstar/scorpiodata/services/MyFirebaseMessagingService.java
@@ -51,13 +51,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        /*Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (/* Check if data needs to be processed by long running job */ true) {
+            if (*//* Check if data needs to be processed by long running job *//* true) {
                 // For long-running tasks (10 seconds or more) use WorkManager.
                 //scheduleJob();
             } else {
@@ -65,13 +65,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 //handleNow();
             }
 
-        }
+        }*/
 
         // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
+        /*if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-
+*/
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
